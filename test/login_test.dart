5f03a9e85ba492826bfa017da50b3403e9ad3f9f
shubham-jain2 <>
2023-12-12 11:31:51 +0530

    - implemented login page unit test cases

---
+++ test/login_test.dart
@@ -0,0 +1,37 @@
+import 'dart:ui';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:online_training_template/generated/l10n.dart';
+import 'package:online_training_template/validators/login_validator.dart';
+
+void main() async {
+  await S.load(Locale.fromSubtags(
+      languageCode: 'en')); // mimic localization delegate init
+
+  group('Login validations test cases', () {
+    test('Empty email', () {
+      expect(LoginValidator(password: '@34ghbsc').validate(),
+          S.current.pleaseEnterEmail);
+    });
+    test('Empty password', () {
+      expect(LoginValidator(email: 'test@gmail.com').validate(),
+          S.current.pleaseEnterPassword);
+    });
+    test('Invalid email', () {
+      expect(
+          LoginValidator(email: 'test@gmail', password: '@aA9hdd').validate(),
+          S.current.pleaseEnterValidEmail);
+    });
+    test('Invalid password', () {
+      expect(
+          LoginValidator(email: 'test@gmail.com', password: 'ww3r').validate(),
+          S.current.passwordMustBeWithIn620Characters);
+    });
+    test('Validation successful.', () {
+      expect(
+          LoginValidator(email: 'test@gmail.com', password: '@aA9hdd')
+              .validate(),
+          S.current.validationSuccessful);
+    });
+  });
+}
